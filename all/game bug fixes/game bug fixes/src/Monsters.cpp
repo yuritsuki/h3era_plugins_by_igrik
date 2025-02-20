3a5b422538ac9090eee5594f175ffd0f212a977e
daemon1995 <63409963+daemon1995@users.noreply.github.com>
2025-02-20 03:16:24 +0300

    fixes update
    - Enchanters now check targets properly;
    - fixed crash in Sacriface dlg at shift right arts button

--- all/game bug fixes/game bug fixes/src/Monsters.cpp
+++ all/game bug fixes/game bug fixes/src/Monsters.cpp
@@ -1,23 +1,28 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int __stdcall setActStack(LoHook* h, HookContext* c)
+int __stdcall setActStack(LoHook *h, HookContext *c)
 {
-    _BattleStack_* mon = (_BattleStack_*)(int)(c->esi -656);
+    _BattleStack_ *mon = (_BattleStack_ *)(int)(c->esi - 656);
 
     // если выстрелов у катапульты или балисты больше нет
     // то делаем пропуски передачи хода этим машинам
-    if (mon->creature_id == 145 || mon->creature_id == 146) {
-        if ( mon->creature.shots < 1 ) {
+    if (mon->creature_id == 145 || mon->creature_id == 146)
+    {
+        if (mon->creature.shots < 1)
+        {
             c->return_address = 0x464DAD;
             return NO_EXEC_DEFAULT;
         }
     }
     // если выстрелов у циклопов больше нет
     // то забираем флаг "катапульта"
-    if (mon->creature_id == 94 || mon->creature_id == 95) {
-        if ( mon->creature.shots < 1 ) {
-            if ( mon->creature.flags & 32 ) {
+    if (mon->creature_id == 94 || mon->creature_id == 95)
+    {
+        if (mon->creature.shots < 1)
+        {
+            if (mon->creature.flags & 32)
+            {
                 mon->creature.flags -= 32;
             }
         }
@@ -26,9 +31,9 @@ int __stdcall setActStack(LoHook* h, HookContext* c)
 }
 
 // создание инкремента боезапаса при атаке осадных стен "катапульта"
-int __stdcall catapultaShoot(LoHook* h, HookContext* c)
+int __stdcall catapultaShoot(LoHook *h, HookContext *c)
 {
-    _BattleStack_* mon = (_BattleStack_*)c->edi;
+    _BattleStack_ *mon = (_BattleStack_ *)c->edi;
     mon->creature.shots -= 1;
 
     return EXEC_DEFAULT;
@@ -36,11 +41,12 @@ int __stdcall catapultaShoot(LoHook* h, HookContext* c)
 
 // запрет на второй выстрел, если боезапас закончился
 // для баллисты и стрелков
-int __stdcall monstreShoot(LoHook* h, HookContext* c)
+int __stdcall monstreShoot(LoHook *h, HookContext *c)
 {
-    _BattleStack_* mon = (_BattleStack_*)c->esi;
+    _BattleStack_ *mon = (_BattleStack_ *)c->esi;
 
-    if (mon->creature.shots < 1) {
+    if (mon->creature.shots < 1)
+    {
         c->return_address = 0x43FFFC;
         return NO_EXEC_DEFAULT;
     }
@@ -51,17 +57,19 @@ int __stdcall monstreShoot(LoHook* h, HookContext* c)
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-_int_ __stdcall Y_FixBagCreatureGredeOfNeutrals(HiHook* hook, _Army_* army, _int_ creature_id)
+_int_ __stdcall Y_FixBagCreatureGredeOfNeutrals(HiHook *hook, _Army_ *army, _int_ creature_id)
 {
     _int_ count = 0;
     _int_ i = 0;
     _int_ crGrade_id = GetCreatureUprade(creature_id);
-    do {
-        if (army->type[i] == creature_id || army->type[i] == crGrade_id) {
+    do
+    {
+        if (army->type[i] == creature_id || army->type[i] == crGrade_id)
+        {
             count += army->count[i];
         }
         i++;
-    } while ( i<7 );
+    } while (i < 7);
 
     return count;
 }
@@ -69,9 +77,10 @@ _int_ __stdcall Y_FixBagCreatureGredeOfNeutrals(HiHook* hook, _Army_* army, _int
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int __stdcall fixHarpyBinds(LoHook* h, HookContext* c)
+int __stdcall fixHarpyBinds(LoHook *h, HookContext *c)
 {
-    if (*(int*)(c->ebx + 696)) {
+    if (*(int *)(c->ebx + 696))
+    {
         c->return_address = 0x478365;
         return NO_EXEC_DEFAULT;
     }
@@ -82,7 +91,7 @@ int __stdcall fixHarpyBinds(LoHook* h, HookContext* c)
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // фикс возвращения Темных Драконов после атаки врага сзади
-_byte_ __stdcall Y_FixMoveDarkDragonsAfterAttack(HiHook* hook, _BattleStack_* stack, _int_ gex, _byte_ a3)
+_byte_ __stdcall Y_FixMoveDarkDragonsAfterAttack(HiHook *hook, _BattleStack_ *stack, _int_ gex, _byte_ a3)
 {
     // проверяем стек на двухклеточность (только у них есть этот баг)
     if (stack->creature.flags & BCF_2HEX_WIDE)
@@ -104,9 +113,10 @@ _byte_ __stdcall Y_FixMoveDarkDragonsAfterAttack(HiHook* hook, _BattleStack_* st
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // не считать кавалерийский бонус при полете
-_int_ __stdcall Y_AntiKavalierAndFly(LoHook* h, HookContext* c)
+_int_ __stdcall Y_AntiKavalierAndFly(LoHook *h, HookContext *c)
 {
-    if ( *(_dword_*)(c->ebx +132) >> 1 & 1 ) { // проверить флаг атакующего на полет
+    if (*(_dword_ *)(c->ebx + 132) >> 1 & 1)
+    {                                 // проверить флаг атакующего на полет
         c->return_address = 0x4430A3; // обходим расчет кавалерийского бонуса (он всё равно не работает)
         return NO_EXEC_DEFAULT;
     }
@@ -116,26 +126,30 @@ _int_ __stdcall Y_AntiKavalierAndFly(LoHook* h, HookContext* c)
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int __cdecl ERM_Fix_EA_E(HiHook* hook, _BattleStack_* stack )
+int __cdecl ERM_Fix_EA_E(HiHook *hook, _BattleStack_ *stack)
 {
     int ret = 0;
     _int32_ spell_duration[81]; // для сохранения длительности заклинаний
     _int32_ spells_power[81];   // для сохранения силы действия заклинания
 
-    if (stack) {
-        for (int i=0; i<80; i++) {
+    if (stack)
+    {
+        for (int i = 0; i < 80; i++)
+        {
             spell_duration[i] = stack->active_spell_duration[i];
             spells_power[i] = stack->active_spells_power[i];
 
-            if (spell_duration[i] > 0 ) // если заклинание наложено на стек, то сбрасываем его эффект
+            if (spell_duration[i] > 0) // если заклинание наложено на стек, то сбрасываем его эффект
                 CALL_2(int, __thiscall, 0x444230, stack, i); // ResetSpellFromStack 0x444230
         }
     }
 
     ret = CALL_1(int, __cdecl, hook->GetDefaultFunc(), stack);
 
-    for (int i=0; i<80; i++) {
-        if (spell_duration[i] > 0) { // если заклинание ранее было наложено, то восстанавливаем его
+    for (int i = 0; i < 80; i++)
+    {
+        if (spell_duration[i] > 0)
+        { // если заклинание ранее было наложено, то восстанавливаем его
             CALL_5(int, __thiscall, 0x444610, stack, i, spell_duration[i], spells_power[i], 0); // ApplySpell 0x444610
         }
     }
@@ -177,31 +191,40 @@ int __cdecl ERM_Fix_EA_E(HiHook* hook, _BattleStack_* stack )
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//#define Wog_FOH_Monstr (*(int*)0x27718CC)
-#define WoG_CreatureUpgradeTable (*(int*)0x724A95)
+// #define Wog_FOH_Monstr (*(int*)0x27718CC)
+#define WoG_CreatureUpgradeTable (*(int *)0x724A95)
 
 // Решаем проблему когда бонусы специалистов не считаются Супер существам
-int __stdcall Y_FixWoG_GetCreatureGrade(LoHook* h, HookContext* c)
+int __stdcall Y_FixWoG_GetCreatureGrade(LoHook *h, HookContext *c)
 {
-    int ID = *(int*)(c->ebp +8);
+    int ID = *(int *)(c->ebp + 8);
     int mon_id = c->ecx;
-    int mon_idGr =  *(int*)(WoG_CreatureUpgradeTable + mon_id*4);
+    int mon_idGr = *(int *)(WoG_CreatureUpgradeTable + mon_id * 4);
 
-    if (mon_idGr == -2) {
+    if (mon_idGr == -2)
+    {
         return EXEC_DEFAULT;
-    } else if (mon_idGr == -1) {
+    }
+    else if (mon_idGr == -1)
+    {
         mon_idGr = CALL_1(int, __fastcall, 0x47AAD0, mon_id);
     }
 
-    int mon_idGr2 = *(int*)(WoG_CreatureUpgradeTable + mon_idGr*4);
+    int mon_idGr2 = *(int *)(WoG_CreatureUpgradeTable + mon_idGr * 4);
 
-    if (mon_idGr2 == -2) {
+    if (mon_idGr2 == -2)
+    {
         return EXEC_DEFAULT;
-    } else if (mon_idGr2 == -1) {
+    }
+    else if (mon_idGr2 == -1)
+    {
         mon_idGr = CALL_1(int, __fastcall, 0x47AAD0, mon_id);
     }
 
-    if (ID == mon_idGr2 ) { c->ecx = mon_idGr; }
+    if (ID == mon_idGr2)
+    {
+        c->ecx = mon_idGr;
+    }
 
     return EXEC_DEFAULT;
 }
@@ -211,52 +234,53 @@ int __stdcall Y_FixWoG_GetCreatureGrade(LoHook* h, HookContext* c)
 
 // корректировка WoG ненависти существ
 // добавляем и существ 8-го уровня
-_int_ __stdcall Y_SetWogHates(LoHook* h, HookContext* c)
+_int_ __stdcall Y_SetWogHates(LoHook *h, HookContext *c)
 {
     int mult = 0; // бонус урона ненависти в процентах
-    int amtype = *(int*)(c->ebp -4);
-    int dmtype = *(int*)(c->ebp -8);
+    int amtype = *(int *)(c->ebp - 4);
+    int dmtype = *(int *)(c->ebp - 8);
 
-    switch(amtype){
-        case 12:  // Ангел
-        case 13:  // Архангел
-        case 150: // Верховный Архангел
-        if(dmtype == 54 || dmtype == 55 || dmtype == 153)
+    switch (amtype)
+    {
+    case 12:  // Ангел
+    case 13:  // Архангел
+    case 150: // Верховный Архангел
+        if (dmtype == 54 || dmtype == 55 || dmtype == 153)
             mult = 50;
-            break;
+        break;
 
-        case 36:  // Джин
-        case 37:  // Мастар Джин
-        if(dmtype == 52 || dmtype == 53)
+    case 36: // Джин
+    case 37: // Мастар Джин
+        if (dmtype == 52 || dmtype == 53)
             mult = 50;
-            break;
+        break;
 
-        case 41:  // Титан
-        case 152: // Громовержец
-        if(dmtype == 83 || dmtype == 155)
+    case 41:  // Титан
+    case 152: // Громовержец
+        if (dmtype == 83 || dmtype == 155)
             mult = 50;
-            break;
+        break;
 
-        case 52:  // Ифрит
-        case 53:  // Ифрит Султан
-        if(dmtype == 36 || dmtype == 37)
+    case 52: // Ифрит
+    case 53: // Ифрит Султан
+        if (dmtype == 36 || dmtype == 37)
             mult = 50;
-            break;
+        break;
 
-        case 54:  // Дьявол
-        case 55:  // Архидьявол
-        case 153: // Барон Ада
-        if(dmtype == 12 || dmtype == 13 || dmtype == 150)
+    case 54:  // Дьявол
+    case 55:  // Архидьявол
+    case 153: // Барон Ада
+        if (dmtype == 12 || dmtype == 13 || dmtype == 150)
             mult = 50;
-            break;
+        break;
 
-        case 83:  // Черный Дракон
-        case 155: // Тёмный Дракон
-        if(dmtype == 41 || dmtype == 152)
+    case 83:  // Черный Дракон
+    case 155: // Тёмный Дракон
+        if (dmtype == 41 || dmtype == 152)
             mult = 50;
-            break;
+        break;
 
-        default:
+    default:
         break;
     }
     // теперь бонус урона нужно занести в EDX
@@ -268,19 +292,22 @@ _int_ __stdcall Y_SetWogHates(LoHook* h, HookContext* c)
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Решение бага ERM: триггер MA:U#/-2 приводил к тому, что любое существо при установке такой команды улучшалось в копейщика
-int __stdcall Fix_WoG_GetCreatureGrade_Expo(LoHook* h, HookContext* c)
+// Решение бага ERM: триггер MA:U#/-2 приводил к тому, что любое существо при установке такой команды улучшалось в
+// копейщика
+int __stdcall Fix_WoG_GetCreatureGrade_Expo(LoHook *h, HookContext *c)
 {
-    if ( *(int*)(c->ebp -4) < -1) {
-        *(int*)(c->ebp -4) = -1;
+    if (*(int *)(c->ebp - 4) < -1)
+    {
+        *(int *)(c->ebp - 4) = -1;
     }
     return EXEC_DEFAULT;
 }
 
-int __stdcall Fix_WoG_GetCreatureGrade_Town(LoHook* h, HookContext* c)
+int __stdcall Fix_WoG_GetCreatureGrade_Town(LoHook *h, HookContext *c)
 {
-    if (*(int*)0x27F93B0 < -1) {
-        *(int*)0x27F93B0 = -1;
+    if (*(int *)0x27F93B0 < -1)
+    {
+        *(int *)0x27F93B0 = -1;
     }
     return EXEC_DEFAULT;
 }
@@ -288,21 +315,21 @@ int __stdcall Fix_WoG_GetCreatureGrade_Town(LoHook* h, HookContext* c)
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define WOG_HeroEntered2Object (*(_Hero_**)0x803300)
+#define WOG_HeroEntered2Object (*(_Hero_ **)0x803300)
 
 // показ лычек опыта в диалоге присоедиения монстров (он же диалог гарнизона)
-int __stdcall Y_Dlg_AddCreatures_Init_Add(HiHook* hook, _Hero_* hero, int creatureType, int creatureCount)
+int __stdcall Y_Dlg_AddCreatures_Init_Add(HiHook *hook, _Hero_ *hero, int creatureType, int creatureCount)
 {
-    if ( hero )
+    if (hero)
         WOG_HeroEntered2Object = hero;
 
     return CALL_3(int, __fastcall, hook->GetDefaultFunc(), hero, creatureType, creatureCount);
 }
 
 // показ лычек опыта в диалоге оставления монстров на карте (он же диалог гарнизона)
-int __stdcall Y_Dlg_AddCreatures_Init_Leave(HiHook* hook, _Hero_* hero, _Army_* army, int creatureType)
+int __stdcall Y_Dlg_AddCreatures_Init_Leave(HiHook *hook, _Hero_ *hero, _Army_ *army, int creatureType)
 {
-    if ( hero )
+    if (hero)
         WOG_HeroEntered2Object = hero;
 
     return CALL_3(int, __fastcall, hook->GetDefaultFunc(), hero, army, creatureType);
@@ -311,18 +338,18 @@ int __stdcall Y_Dlg_AddCreatures_Init_Leave(HiHook* hook, _Hero_* hero, _Army_* 
 // фикс кол-ва артов (опыт существ) при осаде города, когда армия охраны соединяется с армией героя гостя
 _LHF_(Y_Fix_CrExpo_ArtsNum_OnArmy_AddCreatures_BeforeBattleInTown)
 {
-  DwordAt(c->ebp -0x4) = 0;
-  DwordAt(c->ebp -0x24) = 0;
-  return NO_EXEC_DEFAULT;
+    DwordAt(c->ebp - 0x4) = 0;
+    DwordAt(c->ebp - 0x24) = 0;
+    return NO_EXEC_DEFAULT;
 }
 
 // баг, из-за которого палатки могут атаковать вблизи.
 // Причина в построении вектора пути для боевых машин (© daemon_n)
 _LHF_(Gem_OnBattleStackCheckReachability)
 {
-    _BattleStack_* stack = *(_BattleStack_**)(c->ebp +8);
+    _BattleStack_ *stack = *(_BattleStack_ **)(c->ebp + 8);
 
-    if (stack && (stack->creature.flags & BCF_CANT_MOVE || CALL_1(_bool8_, __fastcall, 0x47AAB0, stack->creature_id)) )
+    if (stack && (stack->creature.flags & BCF_CANT_MOVE || CALL_1(_bool8_, __fastcall, 0x47AAB0, stack->creature_id)))
     {
         c->return_address = 0x4B332A;
         return NO_EXEC_DEFAULT;
@@ -331,31 +358,42 @@ _LHF_(Gem_OnBattleStackCheckReachability)
     return EXEC_DEFAULT;
 }
 
-
 // исправление диалога присоединения монстров
 // в Воге нет проверки на присоединение монстров с кол-вом меньше одного
 _LHF_(Y_Fix_CrChangeDialog)
 {
-    for(int i=0; i<7; ++i)
+    for (int i = 0; i < 7; ++i)
     {
-        _int_ adress = (0x83F354 +i*4);
+        _int_ adress = (0x83F354 + i * 4);
         _int_ type = IntAt(adress);
-        _int_ count = IntAt(adress +28);
+        _int_ count = IntAt(adress + 28);
 
         if (type != ID_NONE && count < 1)
         {
             IntAt(adress) = ID_NONE; // type = -1
-            IntAt(adress +28) = 0;   // count = 0
+            IntAt(adress + 28) = 0;  // count = 0
         }
     }
 
     return EXEC_DEFAULT;
 }
-
+// © JackSlater
+// Фикс бага SoD - При проверке возможности наложения заклинаний
+// Чародеи теперь учитывают все отряды, а не только первый с каждой стороны
+_LHF_(gem_EnchantersFindTargetsForSpell)
+{
+    c->edx += c->edi * sizeof(_BattleStack_);
+    return EXEC_DEFAULT;
+}
+_LHF_(gem_EnchantersTryToCastSelectSpell)
+{
+    c->edx += c->esi * sizeof(_BattleStack_);
+    return EXEC_DEFAULT;
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void Monsters(PatcherInstance* _PI)
+void Monsters(PatcherInstance *_PI)
 {
     // исправления стрельбы при отрицат.боезапасе
     // при передаче хода стеку
@@ -391,8 +429,8 @@ void Monsters(PatcherInstance* _PI)
     // не считать кавалерийский бонус при полете
     _PI->WriteLoHook(0x44307A, Y_AntiKavalierAndFly);
 
-    // Решение бага Вога, когда в бою накладывается опыт через EA:E и атака, защита, уроны, скорость, боезапасы и т.п. заново пересчитываются.
-    // Из-за этого теряются бонусы наложенных заклинаний (например бонус скорости от ускорения)
+    // Решение бага Вога, когда в бою накладывается опыт через EA:E и атака, защита, уроны, скорость, боезапасы и т.п.
+    // заново пересчитываются. Из-за этого теряются бонусы наложенных заклинаний (например бонус скорости от ускорения)
     _PI->WriteHiHook(0x726DE4, CALL_, EXTENDED_, CDECL_, ERM_Fix_EA_E);
 
     // фикс переполнения опыта существ (вызов проверки на max опыт)
@@ -401,12 +439,15 @@ void Monsters(PatcherInstance* _PI)
     // вызовы драконов от артефакта сердце дракона
     // меняем местами номера гексов,
     // чтобы в банках существ не перекрывался стек №3
-    _PI->WriteByte(0x767A05 +1, 55);
-    _PI->WriteByte(0x767A2A +1, 89);
+    _PI->WriteByte(0x767A05 + 1, 55);
+    _PI->WriteByte(0x767A2A + 1, 89);
 
-    // Решение бага ERM: триггер MA:U#/-2 приводил к тому, что любое существо при установке такой команды улучшалось в копейщика
-    _PI->WriteDword(0x724A9F, -2);  _PI->WriteLoHook(0x724AC5, Fix_WoG_GetCreatureGrade_Expo);
-    _PI->WriteDword(0x74ED27, -2);  _PI->WriteLoHook(0x74ED5C, Fix_WoG_GetCreatureGrade_Town);
+    // Решение бага ERM: триггер MA:U#/-2 приводил к тому, что любое существо при установке такой команды улучшалось в
+    // копейщика
+    _PI->WriteDword(0x724A9F, -2);
+    _PI->WriteLoHook(0x724AC5, Fix_WoG_GetCreatureGrade_Expo);
+    _PI->WriteDword(0x74ED27, -2);
+    _PI->WriteLoHook(0x74ED5C, Fix_WoG_GetCreatureGrade_Town);
 
     // Решаем проблему когда бонусы специалистов не считаются Супер существам
     // _PI->WriteHiHook(0x4E64FA, CALL_, EXTENDED_, FASTCALL_, Y_FixWoG_GetCreatureGrade);
@@ -433,9 +474,14 @@ void Monsters(PatcherInstance* _PI)
 
     // меняем картинки оставления монстров и для эстетики
     // и для того, чтобы визуально понимать, что мы вносили правку
-    _PI->WriteByte(0x757365 +1, 35); // 35 - Меч Правосудия
-    _PI->WriteByte(0x757369 +1, 13); // 13 - Архангел
+    _PI->WriteByte(0x757365 + 1, 35); // 35 - Меч Правосудия
+    _PI->WriteByte(0x757369 + 1, 13); // 13 - Архангел
 
+    // © JackSlater
+    // Фикс бага SoD - При проверке возможности наложения заклинаний
+    // Чародеи теперь учитывают все отряды, а не только первый с каждой стороны
+    _PI->WriteLoHook(0x0447D9C, gem_EnchantersFindTargetsForSpell);
+    _PI->WriteLoHook(0x0447E4A, gem_EnchantersTryToCastSelectSpell);
 
     // патчи без Tiphon.dll
     if (!TIPHON)
@@ -444,5 +490,4 @@ void Monsters(PatcherInstance* _PI)
         // добавляем и существ 8-го уровня
         _PI->WriteLoHook(0x766E4E, Y_SetWogHates);
     }
-
 }
