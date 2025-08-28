8aff1bdf6679ae05d6aeb8dc307de01dc6777afe
daemon1995 <63409963+daemon1995@users.noreply.github.com>
2025-08-29 02:48:40 +0300

    Update BattleLogDlg.cpp
    // перенос сообщения об уменьшении армии в лог битвы
    // фикс отображения номеров раунда без тактики

--- all/wog native dialogs/wog native dialogs/src/BattleLogDlg.cpp
+++ all/wog native dialogs/wog native dialogs/src/BattleLogDlg.cpp
@@ -91,27 +91,100 @@ int __stdcall BattleLog_Proc(HiHook* hook, _BattleMgr_* bm, _EventMsg_*msg)
 {
     if (!bm->isTactics)
     {
-      if (msg->type == MT_MOUSEBUTTON)
-          if (msg->subtype == MST_LBUTTONCLICK)
-              if ( msg->item_id == 2005 ) 
-                  CreateNewBattleLogDlg(bm);
-
-      if (msg->type == MT_KEYDOWN)
-          if ( msg->subtype == HK_H )
-              CreateNewBattleLogDlg(bm);
+        if (msg->type == MT_MOUSEBUTTON && msg->subtype == MST_LBUTTONCLICK && msg->item_id == 2005
+            || msg->type == MT_KEYDOWN && msg->subtype == HK_H)
+        {
+            CreateNewBattleLogDlg(bm);
+        }
     }
 
     return CALL_2(int, __thiscall, hook->GetDefaultFunc(), bm, msg);
 }
 
+struct RemovedArmy
+{
+    struct
+    {
+        int types[7];
+        int count[7];
+    } sideData[2];
+
+} *removedArmy;
+
+int PrepareRemovedArmyMessage(const int side, char* const buffer)
+{
+    // Формируем сообщение об уменьшении армии
+   
+
+
+   removedArmy = *reinterpret_cast<RemovedArmy**>(0x076C9FE +1);
+   bool isRemoved = false;
+   std::string creatureNames;
+
+//   str.append(text);
+   // итерируем отнятых существ
+   for (size_t i = 0; i < 7; i++)
+   {
+       if (const int removedNum = removedArmy->sideData[side].count[i])
+       {
+		   sprintf(myString3, "%d %s, ", removedNum, o_CreatureInfo->GetName(removedArmy->sideData[side].types[i], removedNum));
+           creatureNames += myString3;
+           // получаем тип существа
+		   isRemoved = true;
+       }
+
+   }
+   if (isRemoved)
+   {
+	   creatureNames.erase(creatureNames.length() - 2);
+	   creatureNames += ".";
+       sprintf(buffer, "%s", creatureNames.c_str());
+
+   }
+
+   return isRemoved;
+}
+
+
+
+// добавление информации о номере раунда в сообщения лога битвы для старта битвы, но без тактики,
+// потому что без неё игра не показывает номер раунда
+void __stdcall BattleMgr_FirstStackTriesToAct(HiHook* hook, _BattleMgr_* bm, const char notFirstRound)
+
+{
+    if (!o_BattleMgr->isTactics && !o_BattleMgr->IsHiddenBattle())
+    {
+
+
+		// сообщение об уменьшении армии
+        for (size_t i = 0; i < 2; i++)
+        {
+            if (PrepareRemovedArmyMessage(i, myString2))
+            {
+                char* text = i ? GetEraJSON("wnd.combat.army_decreased_attacker") : GetEraJSON("wnd.combat.army_decreased_defender");
+
+
+                bm->AddStatusMessage(text);
+
+                bm->AddStatusMessage(myString2);
+            }
+        }
+        char* text = (*reinterpret_cast<_TXT_**>(0x06A5DC4))->GetString(414);
+        sprintf(myString2, "%s {(%d)}", text, 1);
+
+        bm->AddStatusMessage(myString2);
+
+    }
+	CALL_2(void, __thiscall, hook->GetDefaultFunc(), bm, notFirstRound);
+}
 
 void __stdcall BattleLog_ReportNewRound(HiHook* hook, _dword_ battleDlg, const char* textRound, int a3, char a4)
 {
     // o_BattleMgr->round
     _int_ round = Era::v[997];
-    if (round > 0)
+    if (round >= 0)
     {
-        sprintf(myString3, "%s {(%d)}", textRound, round);
+        sprintf(myString3, "%s {(%d)}", textRound, round +1);
         CALL_4(void, __thiscall, hook->GetDefaultFunc(), battleDlg, myString3, a3, a4);
     }    
 }
@@ -125,6 +198,13 @@ void DlgBattleLog(PatcherInstance* _PI)
     // диалог статуса действий и событий в битве
     _PI->WriteHiHook(0x473A00, SPLICE_, EXTENDED_, THISCALL_, BattleLog_Proc);
 
-    // отображение номера раунда в битве
+    // отображение номера раунда в битве и Перенос отображения сообщения об уменьшении вражеской армии командиром Сопряжения (Астральный Дух) в лог битвы
     _PI->WriteHiHook(0x475B19, CALL_, EXTENDED_, THISCALL_, BattleLog_ReportNewRound);
+    _PI->WriteHiHook(0x0462E26, CALL_, EXTENDED_, THISCALL_, BattleMgr_FirstStackTriesToAct);
+
+    // перенос сообщения об уменьшении армии в лог битвы
+	// блокируем оригинальныю функцию для атакующего
+    _PI->WriteJmp(0x076D03B, 0x076D0C9);
+    // блокируем оригинальныю функцию для защитника
+    _PI->WriteJmp(0x076D2CD, 0x076D35B);
 }
