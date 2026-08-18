253109d2ed543dd986a2f060dc277360da4d4f44
Patrick Lin <bitrush2899@gmail.com>
2026-08-18 13:23:29 +0800

    Moved XXL to another repo
    Moved to https://github.com/Archer30/Era-Plugins

--- all/XXL/MapSize.h
+++
@@ -1,7 +0,0 @@
-﻿
-void MapSize_Init();
-void XXLRuntimeFix_Init();
-bool XXLRuntimeFix_EnsureHdCompatibility();
-void XXLRuntimeFix_RefreshTemplateSizes(H3SelectScenarioDialog* dialog, bool correctInvalid);
-void XXLRuntimeFix_BeforeScenarioDialogMessage(H3SelectScenarioDialog* dialog, H3Msg* message);
-void XXLRuntimeFix_AfterScenarioDialogMessage(H3SelectScenarioDialog* dialog);
